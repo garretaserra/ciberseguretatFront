b4a6ebac0f1289c9e27684b93cbe452b00038401
sergi <sergi@unievegroup.com>
2020-03-04 14:49:23 +0100

    added angular material to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TextFieldModule} from '@angular/cdk/text-field';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ciberseguretatFront';
+
+  button(){
+    alert('Clicked button');
+  }
+
 }
