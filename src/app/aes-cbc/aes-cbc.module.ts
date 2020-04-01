d7b01463c3d6bbfb021d9cb8532c5aa6521ad710
Rocío Jamanca <rozzy.1997@gmail.com>
2020-04-01 20:46:34 +0200

    File comented + Importkey function added.

--- src/app/aes-cbc/aes-cbc.module.ts
+++ src/app/aes-cbc/aes-cbc.module.ts
@@ -11,10 +11,22 @@ import {bufToHex, hexToBuf} from "bigint-conversion";
 })
 export class AESCBCModule {
 
-
-
+  /**
+   * Generate a new key (for symmetric algorithms) or key pair (for public-key algorithms).
+   *
+   * Syntax :  const result = crypto.subtle.generateKey(algorithm, extractable, keyUsages);
+   *
+   * @Params{algorithm}  is a dictionary object defining the type of key to generate and providing extra algorithm-specific parameters.
+   *
+   * @Params{extractable} is a Boolean indicating whether it will be possible to export  the key using SubtleCrypto.exportKey() or SubtleCrypto.wrapKey().
+   *
+   * @Params{keyUsages} is an Array indicating what can be done with the newly generated key.
+   *
+   * @returns {Promise} result is a Promise that fulfills with a CryptoKey (for symmetric algorithms) or a CryptoKeyPair (for public-key algorithms).
+   *
+   */
   static async generateKey(){
-   const jwk = await window.crypto.subtle.generateKey(
+    return await window.crypto.subtle.generateKey(
       {
         name: "AES-CBC",
         length: 256, //can be  128, 192, or 256
@@ -22,15 +34,24 @@ export class AESCBCModule {
       true,
       ["encrypt", "decrypt"]
     );
-    return jwk;
   }
 
-  /*
-   Get the encoded message, encrypt it and display a representation
-   of the ciphertext in the "Ciphertext" element.
-   //returns an ArrayBuffer containing the encrypted data
-    */
-
+  /**
+   * The encrypt() method of the SubtleCrypto interface encrypts data.
+   * It takes as its arguments a key to encrypt with, some algorithm-specific parameters, and the data to encrypt (also known as "plaintext").
+   * It returns a Promise which will be fulfilled with the encrypted data (also known as "ciphertext").
+   *
+   * Syntax :  const result = crypto.subtle.encrypt(algorithm, key, data);
+   *
+   * @Params{algorithm} is an object specifying the algorithm to be used and any extra parameters if required.
+   *
+   * @Params{key} is a CryptoKey containing the key to be used for encryption.
+   *
+   * @Params{data} encoded: is a BufferSource containing the data to be encrypted (also known as the plaintext).
+   *
+   * @returns {Promise} encrypted : result is a Promise that fulfills with an ArrayBuffer containing the "ciphertext".
+   *
+   */
   static async encryptMessage(message, key, iv) {
     let enc = new TextEncoder();
     let encoded = enc.encode(message);
@@ -45,10 +66,24 @@ export class AESCBCModule {
     return bufToHex(encrypted);
   }
 
-  /*
-   Fetch the ciphertext and decrypt it.
-   //returns an ArrayBuffer containing the decrypted data
-  */
+  /**
+   * The decrypt() method of the SubtleCrypto interface decrypts some encrypted data.
+   * It takes as arguments a key to decrypt with, some optional extra parameters, and the data to decrypt (also known as "ciphertext").
+   * It returns a Promise which will be fulfilled with the decrypted data (also known as "plaintext").
+   *
+   * Syntax :  const result = crypto.subtle.decrypt(algorithm, key, data);
+   *
+   * @Params{algorithm}  is an object specifying the algorithm to be used, and any extra parameters as required.
+   * The values given for the extra parameters must match those passed into the corresponding encrypt() call.
+   *
+   * @Params{key} is a CryptoKey containing the key to be used for decryption.
+   * If using RSA-OAEP, this is the privateKey property of the CryptoKeyPair object.
+   *
+   * @Params{ciphertext}  is a BufferSource containing the data to be decrypted (also known as ciphertext).
+   *
+   * @returns {Promise} ciphertext : result is a Promise that fulfills with an ArrayBuffer containing the plaintext.
+   *
+   */
   static async decryptMessage(ciphertext, key, iv) {
     ciphertext = hexToBuf(ciphertext);
     return  await window.crypto.subtle.decrypt(
@@ -61,6 +96,43 @@ export class AESCBCModule {
     );
   }
 
-
+  /**
+   * The importKey() method of the SubtleCrypto interface imports a key:
+   * that is, it takes as input a key in an external, portable format and gives you a CryptoKey object that you can use in the Web Crypto API.
+   *
+   * Syntax :  const result = crypto.subtle.importKey(
+                     format,
+                     keyData,
+                     algorithm,
+                     extractable,
+                     usages
+                     );
+   *
+   * @Params{format}  is a string describing the data format of the key to import.
+   *
+   * @Params{keyData} is an ArrayBuffer, a TypedArray, a DataView, or a JSONWebKey object containing the key in the given format.
+   *
+   * @Params{algorithm} is a dictionary object defining the type of key to import and providing extra algorithm-specific parameters.
+   *
+   * @Params{extractable} is a Boolean indicating whether it will be possible to export the key using SubtleCrypto.exportKey() or SubtleCrypto.wrapKey().
+   *
+   * @Params{keyUsages} is an Array indicating what can be done with the key.
+   *
+   * @returns {Promise} result is a Promise that fulfills with the imported key as a CryptoKey object
+   *
+   */
+  static async importKey(key){
+    return await crypto.subtle.importKey(
+      "jwk", {
+        alg: "A256CBC",
+        ext: true,
+        k: key,
+        key_ops: ["encrypt", "decrypt"],
+        kty: "oct",
+      },
+      "AES-CBC",
+      true, //whether the key is extractable (i.e. can be used in exportKey)
+      ["encrypt", "decrypt"]);
+  }
 
 }
