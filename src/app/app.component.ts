5aab6640eb998cfa524681d7695e80f7294cbb0b
garretaserra <garretaserra@gmail.com>
2020-04-01 13:39:35 +0200

    Code refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {GeneralService} from './services/general.service';
 // @ts-ignore
 import my_rsa from 'my_rsa';
 import {bigintToHex, bigintToText, hexToBigint, textToBigint} from 'bigint-conversion';
-import {ChatService} from "./services/chat.service";
+import {ChatService} from './services/chat.service';
 
 
 
@@ -16,29 +16,29 @@ import {ChatService} from "./services/chat.service";
 export class AppComponent {
   rsa;
 
-  //Test Server
+  // Test Server
   testInputText = '';
   testResponseText = '';
 
-  //Blind signature
+  // Blind signature
   serverPublicKey = '';
   blindSignatureRequest = '';
   blindSignatureResponse = '';
   serverE;
   serverN;
-  publicEText= '';
+  publicEText = '';
   verifiedCheckBox = false;
 
-  //No Repudiation
+  // No Repudiation
   username = '';
   userList: User[];
   selectedUser: any = '';
   noRepudiationMessage = '';
   k: bigint;
   c: bigint;
-  Po: bigint;   //Proof of origin
-  Pr: bigint;   //Proof of reception
-  Pkp: bigint;  //Proof of k publication
+  Po: bigint;   // Proof of origin
+  Pr: bigint;   // Proof of reception
+  Pkp: bigint;  // Proof of k publication
 
 
   constructor(
@@ -51,7 +51,7 @@ export class AppComponent {
   ngOnInit() {
     this.handleNewUsers();
     this.handlePrivateMessages();
-    this.handlePublishedMessages()
+    this.handlePublishedMessages();
   }
 
   async getButton() {
@@ -69,8 +69,9 @@ export class AppComponent {
     }
 
     this.publicEText =  bigintToHex(this.serverE);
-    if(event == 'Dec')
-      this.publicEText = parseInt(this.publicEText.toString(),16).toString();
+    if (event === 'Dec') {
+      this.publicEText = parseInt(this.publicEText.toString(), 16).toString();
+    }
   }
 
   async getPublicKeyButton() {
@@ -93,10 +94,10 @@ export class AppComponent {
       return ;
     }
 
-    //Blind message
+    // Blind message
     const res = my_rsa.blind(message, this.serverE, this.serverN);
     const blindedMessage = bigintToHex(res.blindedMessage);
-    //Get r for unblinding
+    // Get r for unblinding
     const r = res.r;
 
     // Make blind signature request to server
@@ -105,7 +106,7 @@ export class AppComponent {
     // UnBlind signature
     blindedSignature = hexToBigint(blindedSignature);
     const signature = my_rsa.unBlind(blindedSignature, r, this.serverN);
-    //Get hex value of the blinded signature so it can be displayed
+    // Get hex value of the blinded signature so it can be displayed
     this.blindSignatureResponse = bigintToHex(signature);
 
     // Verify that the server has signed the message
@@ -128,91 +129,93 @@ export class AppComponent {
     this.selectedUser = user;
   }
 
-  handleNewUsers(){
-    this.ChatService.getConnected().subscribe((message: string)=>{
+  handleNewUsers() {
+    this.ChatService.getConnected().subscribe((message: string) => {
       this.userList = JSON.parse(message);
-      this.userList = this.userList.filter((user)=>{
+      this.userList = this.userList.filter((user) => {
         // user.publicKey = JSON.parse(user.publicKey);
-        if(user.username !== this.username)
+        if (user.username !== this.username) {
           return user;
+        }
       });
-    })
+    });
   }
 
-  startNonRepudiableMessage(){
-    if(!this.selectedUser){
+  startNonRepudiableMessage() {
+    if (!this.selectedUser) {
       alert('Need to select user');
       return;
     }
 
-    if(!this.noRepudiationMessage){
+    if (!this.noRepudiationMessage) {
       alert('Need message to send');
       return;
     }
 
-    //TODO: Symmetrically encrypt message c=m+k
-    //Temporary until above is completed
+    // TODO: Symmetrically encrypt message c=m+k
+    // Temporary until above is completed
     this.c = textToBigint(this.noRepudiationMessage);
 
-    //TODO: Get timestamp
-    let timestamp = 'timestamp here';
+    // TODO: Get timestamp
+    const timestamp = 'timestamp here';
 
-    //Build message
-    let body: NoRepudiationBody = {
+    // Build message
+    const body: NoRepudiationBody = {
       origin: this.username,
       destination: this.selectedUser.username,
       c: bigintToHex(this.c),
-      timestamp: timestamp
+      timestamp
     };
 
-    //TODO: Get Proof of Origin: Sign hash of body
-    let Po = 'Po';
+    // TODO: Get Proof of Origin: Sign hash of body
+    const Po = 'Po';
 
-    let message: NoRepudiationMessage = {
+    const message: NoRepudiationMessage = {
       messageType: 'noRepudiation1',
-      body: body,
+      body,
       signature: Po
     };
 
     this.ChatService.sendMessageToUser(message, this.selectedUser.id);
   }
 
-  handlePrivateMessages(){
-    this.ChatService.privateMessages().subscribe((message: any)=>{
-      if(message.messageType === 'noRepudiation1')
+  handlePrivateMessages() {
+    this.ChatService.privateMessages().subscribe((message: any) => {
+      if (message.messageType === 'noRepudiation1') {
         this.answerNonRepudiableMessage(message);
-      else if (message.messageType === 'noRepudiation2')
+      } else if (message.messageType === 'noRepudiation2') {
         this.publishNoRepudiationMessage(message);
-    })
+ }
+    });
   }
 
-  answerNonRepudiableMessage(message){
-    //Receives fist message of No repudiation from Alice and sends answers with second message to Alice
-    //TODO: Check signature of message sender
+  answerNonRepudiableMessage(message) {
+    // Receives fist message of No repudiation from Alice and sends answers with second message to Alice
+    // TODO: Check signature of message sender
 
     message.body.destination = message.body.origin;
     message.body.origin = this.username;
 
-    //TODO: Get Proof of Reception: Sign the body of the message
+    // TODO: Get Proof of Reception: Sign the body of the message
     message.signature = 'Pr';
 
     message.messageType = 'noRepudiation2';
     this.c = message.body.c;
     message.body.c = undefined;
 
-    //Find destination user
-    this.userList.some((user)=>{
-      if(user.username === message.body.destination){
+    // Find destination user
+    this.userList.some((user) => {
+      if (user.username === message.body.destination) {
         this.ChatService.sendMessageToUser(message, user.id);
         return true;
       }
     });
   }
 
-  publishNoRepudiationMessage(message){
-    //Receives answer from Bob (2nd message of no repudiation) and sends message to publish to TTP (3rd message of no repudiation)
+  publishNoRepudiationMessage(message) {
+    // Receives answer from Bob (2nd message of no repudiation) and sends message to publish to TTP (3rd message of no repudiation)
 
-    //TODO: Check signature
+    // TODO: Check signature
 
     // Refactor message
     message.messageType = 'noRepudiation3';
@@ -221,16 +224,16 @@ export class AppComponent {
     message.body.origin = this.username;
     message.body.k = 'symmetric key';
 
-    //TODO: Sign message body
+    // TODO: Sign message body
     message.signature = 'Pko';
 
     this.ChatService.publishMessage(message);
   }
 
-  handlePublishedMessages(){
-    this.ChatService.receiveBroadcastsNoRepudiation().subscribe((message: any)=>{
+  handlePublishedMessages() {
+    this.ChatService.receiveBroadcastsNoRepudiation().subscribe((message: any) => {
       console.log(message);
-      //TODO: Analyse published message to see if you are Alice or Bob and display info
-    })
+      // TODO: Analyse published message to see if you are Alice or Bob and display info
+    });
   }
 }
