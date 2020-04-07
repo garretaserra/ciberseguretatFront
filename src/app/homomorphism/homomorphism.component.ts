9a2a1468d5c4980809a7534f9a221adbdb465b40
sergi <sergi@unievegroup.com>
2020-04-08 01:12:49 +0200

    Added homomorphism tab

---
+++ src/app/homomorphism/homomorphism.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {AppComponent} from "../app.component";
+
+@Component({
+  selector: 'app-homomorphism',
+  templateUrl: './homomorphism.component.html',
+  styleUrls: ['./homomorphism.component.css']
+})
+export class HomomorphismComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
