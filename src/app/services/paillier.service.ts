f34c8ed0ed8f31d698d661a774117d22df15a29e
sergi <sergi@unievegroup.com>
2020-04-11 21:21:22 +0200

    Added function in service to get key for paillier

--- src/app/services/paillier.service.ts
+++ src/app/services/paillier.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from './environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,15 @@ export class PaillierService {
   constructor(
     public httpClient: HttpClient
   ) { }
+  url = environment.uri;
 
-  private url = 'localhost:3001/';
+  private aggregatorUrl = 'localhost:3001/';
+
+  public getKey(){
+    return this.httpClient.get(this.url + '/calculator/getKey');
+  }
 
   public sum(m1: string, m2:string, n:string){
-    return this.httpClient.post(this.url+'sum', {m1, m2, n})
+    return this.httpClient.post(this.aggregatorUrl+'sum', {m1, m2, n})
   }
 }
