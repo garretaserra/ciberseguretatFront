9a7694cbeff02b59c5221e199a7ee853dfa51e2a
Rocío Jamanca <rozzy.1997@gmail.com>
2020-04-11 23:58:23 +0200

    Paillier Service

--- src/app/homomorphism/homomorphism.component.ts
+++ src/app/homomorphism/homomorphism.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {AppComponent} from "../app.component";
-
+import {GeneralService} from "../services/general.service";
+import {PaillierService} from '../services/paillier.service';
+import {bigintToHex, hexToBigint} from "bigint-conversion";
+import {PublicKey} from "paillier-bigint";
 @Component({
   selector: 'app-homomorphism',
   templateUrl: './homomorphism.component.html',
@@ -11,15 +14,29 @@ export class HomomorphismComponent implements OnInit {
   Number1 = "";
   Number2 = "";
   result = "";
+  publicKey;
 
+  constructor(
+    private paillierService: PaillierService
+  ) {
 
-  constructor() {}
+  }
 
-  ngOnInit() {
+  async ngOnInit() {
+    let response = await this.paillierService.getKey().toPromise();
+    this.publicKey = new PublicKey(response.n,response.g);
   }
 
 
-  Sum() {
+  async Sum() {
+    let n1 = BigInt(this.Number1);
+    let n2 = BigInt(this.Number2);
+
+    let m1 = this.publicKey.encrypt(n1);
+    let m2 = this.publicKey.encrypt(n2);
+
+    let res = await this.paillierService.sum(bigintToHex(m1), bigintToHex(m2), bigintToHex(this.publicKey.n)).toPromise();
+    console.log("res: ",res);
 
   }
 }
--- src/app/services/paillier.service.ts
+++ src/app/services/paillier.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from './environment';
+import {PublicKey} from "paillier-bigint";
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export class PaillierService {
   private aggregatorUrl = 'localhost:3001/';
 
   public getKey(){
-    return this.httpClient.get(this.url + '/calculator/getKey');
+    return this.httpClient.get<PublicKey>(this.url + '/calculator/getKey');
   }
 
   public sum(m1: string, m2:string, n:string){
