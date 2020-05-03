4760c6ee5dec88884d8e3019732a45be852c42b5
sergi <sergi@unievegroup.com>
2020-05-03 12:22:02 +0200

    Started implementation of secre sharing

--- src/app/inheritance/inheritance.component.ts
+++ src/app/inheritance/inheritance.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../models/User";
+import {SymmetricKey} from "../models/SymmetricKey";
+import {bigintToHex} from "bigint-conversion";
+import {MatDialog} from "@angular/material/dialog";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {GeneralService} from "../services/general.service";
+import {ChatService} from "../services/chat.service";
+// @ts-ignore
+import my_rsa from 'my_rsa';
 
 @Component({
   selector: 'app-inheritance',
@@ -7,9 +16,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InheritanceComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    private generalService: GeneralService,
+    private ChatService: ChatService,
+  ) {
+    this.rsa = new my_rsa();
+  }
 
   ngOnInit(): void {
   }
 
+  rsa;
+  username = '';
+  userList: User[];
+  selectedUser: User = {username: undefined, publicKey: undefined, id:undefined};
+  noRepudiationMessage = '';
+  symKey: SymmetricKey;
+  c: bigint;
+  Po: bigint;   //Proof of origin
+  Pr: bigint;   //Proof of reception
+  PrText: string;
+  Pkp: bigint;  //Proof of k publication
+  PkpText: string;
+  lockLoginButton :boolean = false;
+
+  login() {
+    if (!this.username){
+      alert('No username specified');
+      return;
+    }
+    this.ChatService.login(this.username, JSON.stringify({e: bigintToHex(this.rsa.publicKey.e), n: bigintToHex(this.rsa.publicKey.n)}));
+    this.snackBar.open('Logged in','ok', {duration: 1000});
+    this.lockLoginButton = true;
+  }
+
+  selectUser(user) {
+    this.selectedUser = user;
+  }
 }
