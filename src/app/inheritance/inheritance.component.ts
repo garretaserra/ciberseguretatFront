dd2701c882facb3e32ae3f9365c265fc09effce3
sergi <sergi@unievegroup.com>
2020-05-07 18:14:28 +0200

    Removed unused import

--- src/app/inheritance/inheritance.component.ts
+++ src/app/inheritance/inheritance.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../models/User";
-import {SymmetricKey} from "../models/SymmetricKey";
 import {bigintToHex, bufToText, hexToBigint} from "bigint-conversion";
 import {MatDialog} from "@angular/material/dialog";
 import {MatSnackBar} from "@angular/material/snack-bar";
